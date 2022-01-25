73d80211a0fb1c753a82e316543c5cb6d361df47
santjc <scolombatto@lenovo.com>
2022-01-25 16:19:25 -0300

    Add book feature

--- client/app.js
+++ client/app.js
@@ -3,10 +3,12 @@ const ethereumButton = document.getElementById('addWallet');
 
 
 App = {
+    contracts: {},
     init: () => {
-        console.log('Loaded');
         App.loadEthereum();
         App.loadContracts();
+        App.loadAccount();
+        console.log('ECA');
     },
 
     loadEthereum: async () => {
@@ -17,14 +19,33 @@ App = {
             });
             walletAddress.innerText = accounts[0];
             ethereumButton.innerText = "Wallet Connected";
-            console.log("Wallet loaded");
         }
     },
+    loadAccount: async () =>{
+        const accounts = await ethereum.request({
+            method: 'eth_requestAccounts'
+        });
 
+        App.account = accounts[0];
+    },
     loadContracts: async () => {
         const res = await fetch("../build/contracts/BookContracts.json");
         const bookContractJSON = await res.json();
-        console.log(bookContractJSON);
+
+        App.contracts.bookContract = TruffleContract(bookContractJSON);
+
+        App.contracts.bookContract.setProvider(App.web3Provider);
+        App.deployedContract = await App.contracts.bookContract.deployed();
+
+    },
+    renderBooks: () =>{
+
+    },
+    addBook: async (title,desc) => {
+        const result = await App.deployedContract.addBook(title,desc, {
+            from: App.account
+        });
+        console.log(result.logs[0].args);
     }
 }
 
@@ -32,3 +53,13 @@ App = {
 ethereumButton.addEventListener('click', () => {
     App.init();
 });
+
+
+const addBookForm = document.querySelector('#addBookForm');
+addBookForm.addEventListener("submit", e => {
+    e.preventDefault();
+    const bookName = addBookForm['bookName'].value;
+    const bookDesc = addBookForm['bookDesc'].value
+
+    App.addBook(bookName,bookDesc);
+})
